1fee5b761f4f8c7b19ee7332fee3d63ec208cd06
Kalyn Simms <Kalynbsimms@gmail.com>
2020-09-23 02:37:10 -0700

    set up home page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,61 +4,46 @@ import './About.css'
 import { Container, Row, Col } from 'reactstrap';
 import kalyn from '../../components/Assets/Images/kalyn.jpg'
 import { useState } from 'react';
-import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+
 
 const Example = (props) => {
-    const [popoverOpen, setPopoverOpen] = useState(false);
-
-    const toggle = () => setPopoverOpen(!popoverOpen);
+    
     return (
         <div>
-            {/* <Jumbotron> */}
                 <div classname='container'>
                     <img className="size kalyn1" src={kalyn} alt="Kalyn's image" />
                 </div>
                 <Container>
                     <Row>
                         <Col> <h1 className="display-3">Hello,Kalyn Here!</h1>
-                            <p className="lead">I am a Self-motivated Full Stack web developer and systems project coordinator  with a passion for helping individuals t, as well as  directing, coordinating, implementing, executing, controlling and completing Project Implementations. Recently earned a certificate in full stack development from the University of California Irvine, with newly developed skills in JavaScript, CSS, React.js, and responsive web design.  Dedicated team player with exceptional interpersonal skills and a laser focus on business success seeking the position of Front-end Web Developer.</p>
+                            <p className="lead">I am a Self-motivated Full Stack web developer and systems project coordinator  with a passion for helping individuals. I also have a background in  directing, coordinating, implementing, executing, controlling and driving Project Implementations. Recently earned a certificate in full stack development from the University of California Irvine, with newly developed skills in JavaScript, CSS, React.js, and responsive web design.  Dedicated team player with exceptional interpersonal skills and a laser focus on business success.</p>
                             <hr className="my-2" />
-                            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                        <Button id="Popover1" type="button" outline color="warning"  size="lg">LinkedIn!</Button>{' '}
-                            <Button outline color="warning" size="lg">Check out my GitHub!</Button>{' '}
-                        <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-                            <PopoverHeader>Let's Connect</PopoverHeader>
-                            <PopoverBody>Send me a message, I'm always interested in connecting with other developers!</PopoverBody>
-                        </Popover>
-                           
+                            <p>Now that you know a little about me, I would love to hear more about you. Check out ways to keep in touch</p>
+                        <Row>
+                            <Col xs="3">
+                                <Button className="linkBtn" type="button" outline color="warning" size="lg" onClick={(e) => {
+                                    e.preventDefault();
+                                    window.location.href = 'https://www.linkedin.com/in/kalynsimms/'
+                                }}>LinkedIn, Let's Connect!</Button>
+                            </Col>
+                            <Col xs="auto"> </Col>
+                            <Col xs="3">
+                                <Button type="button" outline color="warning" size="lg" onClick={(e) => {
+                                    e.preventDefault();
+                                    window.location.href = 'https://github.com/Kalynbsimms'
+                                }}>GitHub, Let's Collab!</Button>
+                            </Col>
+                        </Row>
+                        
+                        
+                       
                         </Col>
                     </Row>
                     </Container>
-{/*                
-            </Jumbotron> */}
+          
         </div>
     
     );
 };
 
 export default Example;
-
-
-
-// const Example = (props) => {
-//     const [popoverOpen, setPopoverOpen] = useState(false);
-
-//     const toggle = () => setPopoverOpen(!popoverOpen);
-
-//     return (
-//         <div>
-//             <Button >
-//                 Launch Popover
-//       </Button>
-//             <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-//                 <PopoverHeader>Popover Title</PopoverHeader>
-//                 <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
-//             </Popover>
-//         </div>
-//     );
-// }
-
-// export default Example;
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,53 +1,53 @@
-// import React from 'react';
-// import { Card, Button, CardTitle, CardText } from 'reactstrap';
-
-// const Portfolio = (props) => {
-//     return (
-//         <div>
-//             <Card body>
-//                 <CardTitle>Special Title Treatment</CardTitle>
-//                 <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-//                 <Button>Go somewhere</Button>
-//             </Card>
-//             <Card body className="text-center">
-//                 <CardTitle>Special Title Treatment</CardTitle>
-//                 <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-//                 <Button>Go somewhere</Button>
-//             </Card>
-//             <Card body className="text-right">
-//                 <CardTitle>Special Title Treatment</CardTitle>
-//                 <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-//                 <Button>Go somewhere</Button>
-//             </Card>
-//         </div>
-//     );
-// };
-
-// export default Portfolio;
-
 import React from 'react';
 import {
-    Card, CardImg, CardText, CardBody, CardLink,
-    CardTitle, CardSubtitle
+    Card, Button, CardImg, CardTitle, CardText, CardColumns,
+    CardSubtitle, CardBody
 } from 'reactstrap';
 
-const Portfolio = (props) => {
+const Example = (props) => {
     return (
-        <div>
+        <CardColumns>
+            <Card>
+                <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>Pok</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+            <Card>
+                <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+            </Card>
             <Card>
                 <CardBody>
                     <CardTitle>Card title</CardTitle>
                     <CardSubtitle>Card subtitle</CardSubtitle>
-                </CardBody>
-                <img width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-                <CardBody>
-                    <CardText>Some quick Portfolio text to build on the card title and make up the bulk of the card's content.</CardText>
-                    <CardLink href="#">Card Link</CardLink>
-                    <CardLink href="#">Another Link</CardLink>
+                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button>Button</Button>
                 </CardBody>
             </Card>
-        </div>
+            <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Button</Button>
+            </Card>
+            <Card>
+                <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+            <Card body inverse color="primary">
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button color="secondary">Button</Button>
+            </Card>
+        </CardColumns>
     );
 };
 
-export default Portfolio;
+export default Example;
