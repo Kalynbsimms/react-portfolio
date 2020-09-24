26bd1135fbd61be06692c1ed924d1a629406c93c
Kalyn Simms <Kalynbsimms@gmail.com>
2020-09-23 20:48:50 -0700

    updating portfolio

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -3,51 +3,126 @@ import {
     Card, Button, CardImg, CardTitle, CardText, CardColumns,
     CardSubtitle, CardBody
 } from 'reactstrap';
+import pikachu from '../../components/Assets/Images/Pikachu.png'
+import trActivity from '../../components/Assets/Images/trActivity.png'
 
-const Example = (props) => {
+const Portfolio = (props) => {
     return (
         <CardColumns>
             <Card>
-                <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+                <CardImg top width="30%" src={pikachu} alt="Pikachu" />
                 <CardBody>
-                    <CardTitle>Pok</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardTitle>Project 1</CardTitle>
+                    <CardSubtitle>Poke Project</CardSubtitle>
                     <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                    <Button>Button</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://kalynbsimms.github.io/pokeProject/'
+                    }}>Check out the Site!</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://github.com/Kalynbsimms/pokeProject'
+                    }}>Check out the Code!</Button>
                 </CardBody>
             </Card>
             <Card>
-                <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-            </Card>
-            <Card>
+                <CardImg top width="30%" src={trActivity} alt="TrActivity" />
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardTitle>Project 2</CardTitle>
+                    <CardSubtitle>trActivity</CardSubtitle>
                     <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                    <Button>Button</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://cryptic-savannah-70126.herokuapp.com/index.html/'
+                    }}>Check out the Site!</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://github.com/danielle94869/TrActivivy'
+                    }}>Check out the Code!</Button>
                 </CardBody>
-            </Card>
-            <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Button</Button>
             </Card>
             <Card>
-                <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+                <CardImg top width="30%" src={trActivity} alt="TrActivity" />
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-                    <Button>Button</Button>
+                    <CardTitle>Project 2</CardTitle>
+                    <CardSubtitle>trActivity</CardSubtitle>
+                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://cryptic-savannah-70126.herokuapp.com/index.html/'
+                    }}>Check out the Site!</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://github.com/danielle94869/TrActivivy'
+                    }}>Check out the Code!</Button>
                 </CardBody>
             </Card>
-            <Card body inverse color="primary">
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button color="secondary">Button</Button>
+            <Card>
+                <CardImg top width="30%" src="/assets/256x186.svg" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>Project 3</CardTitle>
+                    <CardSubtitle>CodeIn</CardSubtitle>
+                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://cryptic-savannah-70126.herokuapp.com/index.html/'
+                    }}>Check out the Site!</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://github.com/danielle94869/TrActivivy'
+                    }}>Check out the Code!</Button>
+                </CardBody>
             </Card>
+            <Card>
+                <CardImg top width="30%" src={trActivity} alt="TrActivity" />
+                <CardBody>
+                    <CardTitle>Project 2</CardTitle>
+                    <CardSubtitle>trActivity</CardSubtitle>
+                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://cryptic-savannah-70126.herokuapp.com/index.html/'
+                    }}>Check out the Site!</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://github.com/danielle94869/TrActivivy'
+                    }}>Check out the Code!</Button>
+                </CardBody>
+            </Card>
+            <Card>
+                <CardImg top width="30%" src={trActivity} alt="TrActivity" />
+                <CardBody>
+                    <CardTitle>Project 2</CardTitle>
+                    <CardSubtitle>trActivity</CardSubtitle>
+                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://cryptic-savannah-70126.herokuapp.com/index.html/'
+                    }}>Check out the Site!</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://github.com/danielle94869/TrActivivy'
+                    }}>Check out the Code!</Button>
+                </CardBody>
+            </Card>  <Card>
+                <CardImg top width="30%" src={trActivity} alt="TrActivity" />
+                <CardBody>
+                    <CardTitle>Project 2</CardTitle>
+                    <CardSubtitle>trActivity</CardSubtitle>
+                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://cryptic-savannah-70126.herokuapp.com/index.html/'
+                    }}>Check out the Site!</Button>
+                    <Button onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = 'https://github.com/danielle94869/TrActivivy'
+                    }}>Check out the Code!</Button>
+                </CardBody>
+            </Card>
+            
         </CardColumns>
     );
 };
 
-export default Example;
+export default Portfolio;
