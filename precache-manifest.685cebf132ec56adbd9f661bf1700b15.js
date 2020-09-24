e323ccb7cf553607d459df5924b92c546aa8b878
Kalyn Simms <Kalynbsimms@gmail.com>
2020-09-23 20:59:47 -0700

    Updates

---
+++ precache-manifest.685cebf132ec56adbd9f661bf1700b15.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0362bb1de27db07a0999dcb6cc8eb681",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f99aa7ba0c60b70bb3ca",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "28289d3177265667cb7b",
+    "url": "/react-portfolio/static/css/main.a5703282.chunk.css"
+  },
+  {
+    "revision": "f99aa7ba0c60b70bb3ca",
+    "url": "/react-portfolio/static/js/2.b0e1635e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.b0e1635e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28289d3177265667cb7b",
+    "url": "/react-portfolio/static/js/main.833e2364.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "f63f085046f4a0b6d2437b3b28c0d6fd",
+    "url": "/react-portfolio/static/media/Pikachu.f63f0850.png"
+  },
+  {
+    "revision": "c2829ec64be0638605b381b9ecda2b3b",
+    "url": "/react-portfolio/static/media/kalyn.c2829ec6.jpg"
+  },
+  {
+    "revision": "7ada72737e2bad253e3ca7dcd299b9a5",
+    "url": "/react-portfolio/static/media/trActivity.7ada7273.png"
+  }
+]);
